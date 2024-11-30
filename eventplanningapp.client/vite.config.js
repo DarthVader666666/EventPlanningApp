3f39e8571ad375b23d8b058bb28021282b388832
CBT0\V.Rumyantsev <V.Rumyantsev@cbt.by>
2024-11-30 19:16:16 +0300

    Defined server url for production

--- eventplanningapp.client/vite.config.js
+++ eventplanningapp.client/vite.config.js
@@ -30,8 +30,10 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7198';
+const target =
+    //env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    //env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7198';
+    env.MODE === 'development' ? 'https://localhost:7198' : 'https://event-planning-app.azurewebsites.net';
 
 // https://vitejs.dev/config/
 export default defineConfig({
