7bc054548e6918f57d3edbd7efab7218c7a70173
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-12-02 17:01:06 +0300

    Adjusted base path for Routes

--- eventplanningapp.client/src/App.jsx
+++ eventplanningapp.client/src/App.jsx
@@ -8,18 +8,20 @@ import Confirm from './Confirm.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const basePath =  (import.meta.env.MODE === 'development' ? '' : 'https://event-planning-app.azurewebsites.net');
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/events/create" element={<Create/>}></Route>
-            <Route path="/login/" element={<LogIn/>}></Route>
-            <Route path="/register/" element={<Register/>}></Route>
-            <Route path="/events/:eventId" element={<EventDetails />}></Route>
-            <Route path="/confirm/:userId/:eventId" element={<Confirm />}></Route>
+            <Route path={basePath + "/"} element={<Home/>}></Route>
+            <Route path={basePath + "/events/create"} element={<Create/>}></Route>
+            <Route path={basePath + "/login/"} element={<LogIn/>}></Route>
+            <Route path={basePath + "/register/"} element={<Register/>}></Route>
+            <Route path={basePath + "/events/:eventId"} element={<EventDetails />}></Route>
+            <Route path={basePath + "/confirm/:userId/:eventId"} element={<Confirm />}></Route>
           </Routes>
         </div>
       </div>
