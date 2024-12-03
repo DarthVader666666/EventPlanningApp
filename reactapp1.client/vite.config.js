0c4b415aa796dd73e3c4b73075b4876cf79e54b7
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-12-03 14:31:02 +0300

    Refactored certificate file path definition

--- reactapp1.client/vite.config.js
+++ reactapp1.client/vite.config.js
@@ -3,18 +3,18 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import fs from 'fs';
-import path from 'path';
+//import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+//const baseFolder =
+//    env.APPDATA !== undefined && env.APPDATA !== ''
+//        ? `${env.APPDATA}/ASP.NET/https`
+//        : `${env.HOME}/.aspnet/https`;
 
 const certificateName = "reactapp1.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+const certFilePath = `${certificateName}.pem`;
+const keyFilePath = `${certificateName}.key`;
 
 if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     if (0 !== child_process.spawnSync('dotnet', [
