cd412bb91b3b4dbd5f277b3eda40851ba5e72427
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-12-04 15:12:08 +0300

    Added folder with dll

---
+++ EventPlanning.Bll/Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Communication.Email;
+using Microsoft.Extensions.Configuration;
+
+namespace EventPlanning.Bll.Services
+{
+    public class EmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<EmailSendOperation?> SendEmailAsync(string email, string subject, string message)
+        {
+            var sender = _configuration["AzureEmailSender"];
+            var connectionString = _configuration["ConnectionStrings:AzureCommunicationService"];
+
+            var client = new EmailClient(connectionString);
+
+            EmailSendOperation? result = null;
+
+            try
+            {
+                var task = client.SendAsync(Azure.WaitUntil.Completed, sender, email, subject, message);
+                result = await task;
+            }
+            catch (Exception ex) 
+            {
+                Console.Write(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
