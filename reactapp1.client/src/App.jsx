96e491d24f053c74f8c924590108f5a41f1ab121
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-12-04 09:27:31 +0300

    Rolled back last changes

--- reactapp1.client/src/App.jsx
+++ reactapp1.client/src/App.jsx
@@ -1,15 +1,22 @@
 import Home from './Home.jsx';
-//import Navbar from './Navbar.jsx';
+import Navbar from './Navbar.jsx';
 //import Create from './Create.jsx';
 //import LogIn from './Login.jsx';
 //import Register from './Register.jsx';
 //import EventDetails from './EventDetails.jsx';
 //import Confirm from './Confirm.jsx';
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <Home/>
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <div className="content">
+                    <Home/>
+                </div>
+            </div>    
+        </Router>
   );
 }
 
