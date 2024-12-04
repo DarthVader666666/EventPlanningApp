85e9243e1dd0c5613a25e9f1f375fab2ff948e3d
V.Rumyantsev <V.Rumyantsev@48-1335>
2024-12-04 10:07:55 +0300

    Implemented JWT Authentication

--- ReactApp1.Server/Program.cs
+++ ReactApp1.Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = "Test",
+        ValidAudience = "Test",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Test")),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+    };
+});
+
 
 var app = builder.Build();
 
